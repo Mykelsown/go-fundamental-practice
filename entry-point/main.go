bfd96e77b0b50793fee7008c49a0a402960b450d
micsamuel <mykelsown@gmail.com>
2026-04-08 17:24:01 +0100

    Restructured the files directory

--- entry-point/main.go
+++ entry-point/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"fmt"
-	"practice"
+	method "practice/methods"
 )
 
 func main() {
 	fmt.Println("..........Let Get Things Started 😋..........")
 	var age int = 10
 	var text string = "Shola is "
-	practice.ModifiedPointer(&age, &text)
+	method.ModifiedPointer(&age, &text)
 	fmt.Print("\n-------------------------------------------------\n\n")
-	// practice.WorkingSlices()
-	fmt.Println(practice.Solution("world"))
+	// method.WorkingSlices()
+	fmt.Println(method.Solution("world"))
 
 	arr1 := []bool{
       true,  true,  true,  false,
@@ -22,10 +22,10 @@ func main() {
       true,  true,  true,  true ,
       false, false, true,  true,
     }
-	fmt.Println(practice.CountSheeps(arr1))
+	fmt.Println(method.CountSheeps(arr1))
 
 	char := "bkko"
-	fmt.Println(practice.RemoveChar(char))
+	fmt.Println(method.RemoveChar(char))
 
-	practice.ToRecover()
+	method.ToRecover()
 }
