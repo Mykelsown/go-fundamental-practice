7ff71ae6f685a8f1e90724f6b397c05d2068ea72
micsamuel <mykelsown@gmail.com>
2026-04-08 17:35:24 +0100

    Half done for working with composition

--- methods/working-with-composition.go
+++ methods/working-with-composition.go
@@ -1,1 +1,17 @@
 package method
+
+type fellowAddress struct{
+	country string
+	state string
+	LGA string
+	town string
+}
+
+var call = fellowAddress{
+	country: "Nigeria",
+	state: "Lagos",
+	LGA: "Ikorodu",
+	town: "Adamo",
+}
+func (call fellowAddress)
+
