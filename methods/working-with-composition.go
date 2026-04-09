6b0eb4b2853a3dbdfeb7f34ed42d731e82a67c17
micsamuel <mykelsown@gmail.com>
2026-04-09 12:20:06 +0100

    I'm getting confused, i will continue later from home

--- methods/working-with-composition.go
+++ methods/working-with-composition.go
@@ -1,17 +1,51 @@
 package method
 
-type fellowAddress struct{
+import "fmt"
+
+type address struct {
 	country string
-	state string
-	LGA string
-	town string
+	state   string
+	LGA     string
+	town    string
 }
 
-var call = fellowAddress{
+
+func (f address) PrintDetails() {
+	fmt.Println(f.country)
+	fmt.Println(f.state)
+	fmt.Println(f.LGA)
+	fmt.Println(f.town)
+}
+
+var callFellow = address{
 	country: "Nigeria",
-	state: "Lagos",
-	LGA: "Ikorodu",
-	town: "Adamo",
+	state:   "Lagos",
+	LGA:     "Ikorodu",
+	town:    "Adamo",
 }
-func (call fellowAddress)
 
+var callCompany = address{
+	country: "England",
+	state:   "Yorkshire",
+	LGA:     "Wendy",
+	town:    "Loomside",
+}
+type employee struct {
+	name       string
+	department string
+	age        int
+	faddress   address
+	district   address
+}
+
+var empDetails = employee{
+	name:       "Micheal Samuel",
+	department: "Engineering",
+	age:        14,
+	fAddress:  address{
+		country: "Nigeria",
+	} ,
+	cAddress: address{
+		country: ,
+	},
+}
